b268d15fdc2c2d789d115a39be5057acf18f1f0d
方继略 <fangjilve@mogoroom.com>
2019-04-24 21:14:32 +0800

    支持404,500

--- src/main/java/me/robbie/spring/demo/action/WebAction.java
+++ src/main/java/me/robbie/spring/demo/action/WebAction.java
@@ -1,10 +1,12 @@
 package me.robbie.spring.demo.action;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -18,22 +20,29 @@ import java.util.Date;
  * @since [产品/模块版本]
  */
 @Controller
-@RequestMapping(value = "/web")
 public class WebAction {
 
-    @RequestMapping(value = "index",method = RequestMethod.GET)
+    @RequestMapping(value = "/web",method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("date",new Date().toInstant());
         return "index@layout/layout";
     }
 
-    @RequestMapping(value = "user",method = RequestMethod.GET)
+    @RequestMapping(value = "/web/user",method = RequestMethod.GET)
     public String userList(Model model) {
         ArrayList<String> arr = new ArrayList<>();
         arr.add("zhangzq");
         arr.add("licm");
 
         model.addAttribute("arr",arr);
+        return "module/user_list@layout/layout";
+    }
+
+
+    @RequestMapping(value = "/web/user1",method = RequestMethod.GET)
+    public String userList2(Model model) {
+        Integer i = null;
+        i.toString();
         return "module/user_list@layout/layout";
     }
 }
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,4 @@
+function indexLoad(){
+
+    console.log('index  .....');
+}
